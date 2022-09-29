d7721d8023990fe94a176d0bc40b44b17e96dc92
Caio Fernandes <caio.fernandes.1216303@sga.pucminas.br>
2022-09-29 14:50:49 -0300

    inserindo os gráficos do relatório do LAB6

--- codigo/LAB6/Program.cs
+++ codigo/LAB6/Program.cs
@@ -23,28 +23,37 @@
     }
 
     static void BubbleSort(int[] vetor){
+        int trocas = 0, comparacoes = 0;
         for(int i = vetor.Length - 1; i > 1; i--){
             for(int j = 0; j < i; j++){
                 if(vetor[j] > vetor[j + 1]){
                     swap(vetor, j, j + 1);
+                    trocas++;
                 }
+                comparacoes++;
             }
         }
+        Console.Write($"Comparações = {comparacoes}\nTrocas = {trocas}\n\n");
     }
 
     static void SelectionSort(int[] vetor){
+        int trocas = 0, comparacoes = 0;
         for(int i = 0; i < vetor.Length - 2; i++){
             int menor = i;
             for(int j = i + 1; j < vetor.Length - 1; j++){
                 if(vetor[j] < vetor[menor]){
                     menor = j;
                 }
+                comparacoes++;
             }
             swap(vetor, i, menor);
+            trocas++;
         }
+        Console.Write($"Comparações = {comparacoes}\nTrocas = {trocas}\n\n");
     }
 
     static void InsertionSort(int[] vetor){
+        int trocas = 0, comparacoes = 0;
         for(int i = 1; i < vetor.Length; i++){
             int aux = 0, troca = 0;
             int j = i - 1;
@@ -53,20 +62,26 @@
                 if(j == 0){
                     if(vetor[j] > vetor[i]){
                         troca++;
+                        trocas += troca;
                     }
                     aux = 1;
+                    comparacoes++;
                 }
                 else if(vetor[j] < vetor[i]){
                     aux = 1;
+                    comparacoes+= 2;
                 }
                 else{
                     j--;
                     troca++;
+                    trocas+= troca;
+                    comparacoes+= 2;
                 }
             }
 
             insert(vetor, i - troca, i);
         }
+        Console.Write($"Comparações = {comparacoes}\nTrocas = {trocas}\n\n");
     }
 
     static void Main(string[] args){
