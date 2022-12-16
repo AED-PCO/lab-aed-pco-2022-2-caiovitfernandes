e39bf8e58ae85409b7ab2d9dc572cf4474e4d90c
Caio Fernandes <caio.fernandes.1216303@sga.pucminas.br>
2022-12-15 21:32:30 -0300

    commit final

---
+++ codigo/LAB11/metodos.cs
@@ -0,0 +1,17 @@
+namespace estruturas{
+    class listaCircEstatica{
+        public static void inserir(int[] valor, ref int referencia, int pos){
+            
+        }
+
+        
+    }
+
+    class listaCiscDinamica{
+
+    }
+
+    class listaDpEncadeada{
+
+    }
+}
